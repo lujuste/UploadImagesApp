7e6124deba9fa48e0fdac82bba899b5dcdd81a79
Lucas Juste <lu.scatollini@hotmail.com>
2021-08-27 01:09:09 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,87 @@
+/* eslint-disable prettier/prettier */
+import { Button, Box } from '@chakra-ui/react';
+import { useMemo } from 'react';
+import { useInfiniteQuery } from 'react-query';
+import { Header } from '../components/Header';
+import { CardList } from '../components/CardList';
+import { api } from '../services/api';
+import { Loading } from '../components/Loading';
+import { Error } from '../components/Error';
+
+interface Image {
+  title: string;
+  description: string;
+  url: string;
+  ts: number;
+  id: string;
+}
+
+interface GetImagesResponse {
+  after: string;
+  data: Image[];
+}
+
+export default function Home(): JSX.Element {
+  async function fetchImages({ pageParam = null }): Promise<GetImagesResponse> {
+    const { data } = await api.get('/api/images', {
+      params: {
+        after: pageParam,
+      },
+    });
+    return data;
+  }
+
+  const {
+    data,
+    isLoading,
+    isError,
+    isFetchingNextPage,
+    fetchNextPage,
+    hasNextPage,
+  } = useInfiniteQuery(
+    'images',
+    fetchImages,
+    {
+      getNextPageParam: lastPage => lastPage?.after || null,
+    }
+    // TODO AXIOS REQUEST WITH PARAM
+    // TODO GET AND RETURN NEXT PAGE PARAM
+  );
+
+  const formattedData = useMemo(() => {
+    const formatted = data?.pages.flatMap(imageData => {
+      return imageData.data.flat();
+    });
+    console.log(data?.pages);
+    console.log(formatted);
+    return formatted;
+  }, [data]);
+
+  
+    // TODO RENDER LOADING SCREEN
+
+    if (isLoading && !isError) {
+      return( <Loading />)
+    }
+
+    // TODO RENDER ERROR SCREEN
+
+    if (!isLoading && isError) {
+      return( <Error />)
+    }
+
+    return (
+      <>
+        <Header />
+
+        <Box maxW={1120} px={20} mx="auto" my={20}>
+          <CardList cards={formattedData} />
+          {hasNextPage && (
+            <Button onClick={() => fetchNextPage()} disabled={isFetchingNextPage} mt="4">
+              {isFetchingNextPage ? 'Carregando' :'Carregar mais'}
+            </Button>
+          )}
+        </Box>
+      </>
+    );
+}
